f7f47b7e8f961f14b3a14028132af127b47a6132
konduri123 <47478571+konduri123@users.noreply.github.com>
2019-06-16 13:57:30 +0530

    Add files via upload

--- src/main/java/com/virtusa/test/Application.java
+++ src/main/java/com/virtusa/test/Application.java
@@ -1,1 +1,36 @@
+package com.virtusa.test;
 
+import java.util.ArrayList;
+
+/**
+ * @author DharmaTeja
+ * <p>This class is the starting point of the application.<br>
+ * Kindly see the <b>package-info.java</b> for assumptions made in the application</p>
+ * 
+ */
+public class Application {
+
+	/**
+	 * <p>Entry point of the application</p>
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		Utility.println("Welcome to GalaxyMerchant ! please provide input below and a blank new line to finish input");
+		
+		// Initialize a new paragraph
+		Paragraph paragraph = new Paragraph(false,null);
+		
+		// Read the input from console, validate and process
+		ArrayList<String> output=paragraph.read();
+		
+		for(int i=0;i<output.size();i++)
+		{
+			Utility.println(output.get(i));
+		}
+		
+		
+	}
+
+}
